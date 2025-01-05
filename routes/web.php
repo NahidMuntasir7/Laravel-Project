734f92c39c254161d695905115b934c1a3efe090
Nahid Muntasir <nahidmuntasir2@gmail.com>
2025-01-05 18:54:45 +0600

    my commit message

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'hello world';
 });
+Route::get('/greeting', function () {
+    return view('greeting');
+});
+ 
+Route::get('/users', function () {
+    $users = ['john', 'jane', 'joe'];
+    return view('users', compact('users'));
+});
